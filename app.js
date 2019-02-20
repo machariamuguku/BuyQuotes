7e05a38cd16f4bddc4444ad3a263c3c589c813de
Muguku <machariamuguku@gmail.com>
2019-02-20 12:34:21 +0300

    added other dependencies

--- app.js
+++ app.js
@@ -2,14 +2,34 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
+
+// add moongose and connect to db
+mongoose.connect("mongodb://localhost:27017/buyquotes", {
+    useNewUrlParser: true
+}).then(
+    () => {
+        console.log('Database connection is successful')
+    },
+    err => {
+        console.log('Error when connecting to the database' + err)
+    }
+);
 
 // Require router module here ...
 const router = require('./router');
 
 const app = express();
 
+// add bodyparser and cors
+app.use(bodyParser.json());
+app.use(cors());
+
 // const port = process.env.PORT;
-const port = 1800;
+const port = process.env.PORT || 1800;
 
 app.set('port', port);
 
@@ -22,9 +42,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Set resources folder
 app.use(express.static(path.join(__dirname, "resources")));
-
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
@@ -44,7 +62,6 @@ app.use((err, req, res) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     res.status(err.status || 500);
     res.render('error');
 });
