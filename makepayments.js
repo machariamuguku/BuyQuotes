fb4016876f711f263c0242d04339aa3935793813
Muguku <machariamuguku@gmail.com>
2019-02-21 00:54:01 +0300

    added verification

--- makepayments.js
+++ makepayments.js
@@ -1,43 +1,42 @@
-module.exports = {
-    // Function 1: generate access token
-    generateAcessToken: function () {
-        var accessToken;
-        var request = require('request'),
-            consumer_key = "9cTmL66nSbBGUHpnDJoxzjpiGV7SAd9N",
-            consumer_secret = "TEYbiahbnSmUErPV",
-            url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-        auth = "Basic " + new Buffer(consumer_key + ":" + consumer_secret).toString("base64");
+// Function 1: generate access token
+generateAcessToken = () => {
+    var accessToken;
+    var request = require('request'),
+        consumer_key = "9cTmL66nSbBGUHpnDJoxzjpiGV7SAd9N",
+        consumer_secret = "TEYbiahbnSmUErPV",
+        url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    auth = "Basic " + new Buffer(consumer_key + ":" + consumer_secret).toString("base64");
 
-        request({
-                url: url,
-                headers: {
-                    "Authorization": auth
-                }
-            },
-            function (error, response, body) {
-
-                if (error) {
-                    error = JSON.parse(error);
-                    console.log(error);
-                } else {
-                    // TODO: Use the body object to extract OAuth access token
-                    body = JSON.parse(body);
-                    accessToken = body.access_token;
-                    console.log(accessToken);
-                }
-
+    request({
+            url: url,
+            headers: {
+                "Authorization": auth
             }
-        )
+        },
+        function (error, response, body) {
 
-        return accessToken;
-    },
+            if (error) {
+                console.log(error);
+            } else {
+                // TODO: Use the body object to extract OAuth access token
+                body = JSON.parse(body);
+                accessToken = body.access_token;
+                console.log("method1 "+accessToken);
+            }
+
+        }
+    )
+
+};
+
+module.exports = {
     // Function 2: generate ValidationURL
     generateValidationURL: function () {
-        var accessToken = generateAcessToken();
+        let accessToken = generateAcessToken();
         var request = require('request'),
             oauth_token = accessToken,
             url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-        auth = "Bearer " + oauth_token;
+        auth = "Bearer " + accessToken;
 
         request({
                 method: 'POST',
@@ -54,29 +53,10 @@ module.exports = {
             },
             function (error, response, body) {
                 // TODO: Use the body object to extract the 
-                body = JSON.parse(body)
-                console.log(body);
+                body = JSON.stringify(body);
+                console.log("method2 "+body);
+
             }
         )
     },
 };
-
-// var zemba = function () {
-//     // create our webhook endpoint to recive webhooks from Safaricom
-//     app.post('/hooks/mpesa', (req, res) => {
-
-//         console.log('-----------Received M-Pesa webhook-----------');
-
-//         // format and dump the request payload recieved from safaricom in the terminal
-//         console.log(prettyjson.render(req.body, options));
-//         console.log('-----------------------');
-
-//         let message = {
-//             "ResponseCode": "00000000",
-//             "ResponseDesc": "success"
-//         };
-
-//         // respond to safaricom servers with a success message
-//         res.json(message);
-//     });
-// }
