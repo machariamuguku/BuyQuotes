b4dc697dea745e13d548c3902ca9025473029cb2
Muguku <machariamuguku@gmail.com>
2019-02-27 21:28:28 +0300

    dd

--- router.js
+++ router.js
@@ -171,7 +171,7 @@ router.post("/lipanampesa/success", (req, res) => {
   let lipanampesaAllResponse = req.body;
   if (lipanampesaAllResponse) {
     console.log('it should work!');
-    console.log(req);
+    console.log(req.body);
     res.redirect('/youpaid');
   }
   // res.render("cart", {
