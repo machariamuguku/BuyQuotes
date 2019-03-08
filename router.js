ce0016a076aadeed30e4235ec54e409ea34bc791
Muguku <machariamuguku@gmail.com>
2019-03-09 01:02:25 +0300

    last try

--- router.js
+++ router.js
@@ -263,10 +263,8 @@ router.post("/lipanampesa/success", (req, res) => {
       if (err) throw new Error(err.message, null);
       emailobjects = res;
       let getQuotes = require('./thequotes');
-      let quotecategory1 = emailobjects.quotecategory;
-      let getQuotesArguments = (quotecategory1+".quotes");
-      console.log(getQuotesArguments);
-      quotesobjects = getQuotes(getQuotesArguments); //look at thequotes.js to understand the arguments 
+     // let getQuotesArguments = (emailobjects.quotecategory + ".quotes");
+      quotesobjects = getQuotes(emailobjects.quotecategory + ".quotes"); //look at thequotes.js to understand the arguments 
       //array containing quote objects
       //set the email objects with response
       const sendTheEmail = require("./sendemail.js"); //call sendemail.js
