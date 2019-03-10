b8ac9b4db553be58f5253cc451c0b36ebdacc0dc
Muguku <machariamuguku@gmail.com>
2019-03-10 17:39:53 +0300

    to string

--- router.js
+++ router.js
@@ -263,7 +263,7 @@ router.post("/lipanampesa/success", (req, res) => {
       if (err) throw new Error(err.message, null);
       emailobjects = res;
       let getQuotes = require('./thequotes');
-      quotesobjects = getQuotes(emailobjects.quotecategory + ".quotes").toString; //look at thequotes.js to understand the arguments
+      quotesobjects = getQuotes((emailobjects.quotecategory + ".quotes".toString)); //look at thequotes.js to understand the arguments
       console.log(quotesobjects);
       //let quotesobjects = getQuotes(Programming.quotes); 
       //array containing quote objects
